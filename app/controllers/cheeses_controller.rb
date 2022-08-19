2ab669ce195217925bc147c4c3549756f1264d4a
Jaime Montes de Oca <jaime0422@live.com>
2022-08-18 23:09:06 -0700

    Completed Assignment

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -1,8 +1,21 @@
 class CheesesController < ApplicationController
 
-  def index
-    cheeses = Cheese.all
-    render json: cheeses
-  end
+    def index
+        cheeses = Cheese.all
+        render json: cheeses
+    end
+
+    def show
+        cheese = Cheese.find(params[:cheese])
+        render json: cheese
+    end
+
+    def this_cheese
+        render json: Cheese.find_by_name(params[:name])
+    end
+
+    def how_many_cheeses
+        render json: Cheese.limit(params[:how_many])
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  get '/cheeses', to: 'cheeses#index'
+
+    get '/cheeses', to: 'cheeses#index'
+
+    get '/cheeses/:cheese', to: 'cheeses#show'
+
+    get '/this-cheese/:name', to: 'cheeses#this_cheese'
+
+    get '/quesos-todos/:name', to: 'cheeses#this_cheese'
+
+    get '/how-many-cheeses/:how_many', to: 'cheeses#how_many_cheeses'
+
 end
